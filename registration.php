fe17bdbd38a88a029bf70010543c2e9054414058
ShopWhizzy <info@shopwhizzy.com>
2026-07-13 19:22:05 +0200

    Rename module to ShopWhizzy_StripeHostedCheckout

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ShopWhizzy_StripeCheckout', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ShopWhizzy_StripeHostedCheckout', __DIR__);
